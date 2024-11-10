20112b1cc762a2d04400fc1d12d7de8219e925e2
Jackie Huynh <39444662+AJ7766@users.noreply.github.com>
2024-11-10 22:01:52 +0100

    Update sessions.ts

--- server/src/utils/sessions.ts
+++ server/src/utils/sessions.ts
@@ -6,5 +6,6 @@ export const assignCookieSession = async (req: Request, user_id: mongoose.Types.
     if (!req.session) {
         throw new Error("Session is not available on the request object");
     }
+    console.log("Session ID:", req.sessionID)
     req.session.user_id = user_id;
 }
